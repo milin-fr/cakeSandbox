7d3a22e0da6f7481daac9f9ca425eca687d06de0
unknown <m.ilin.fr@gmail.com>
2020-06-08 11:03:02 +0200

    drafting index action

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -5,4 +5,10 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+    public function index()
+    {
+        $this->loadComponent('Paginator');
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));
+    }
 }
